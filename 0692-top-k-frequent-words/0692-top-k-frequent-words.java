8867da3b7e48f1e17a94f885d64360b7d3232ff8
KrishO9 <84735625+KrishO9@users.noreply.github.com>
2024-02-28 11:42:23 +0530

    Time: 44 ms (5.54%), Space: 45.1 MB (8.96%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -4,54 +4,56 @@ import java.util.*;
 import java.util.ArrayList;
 import java.util.Comparator;
 class Solution {
-          public List<String> topKFrequent(String[] words, int k) {
-        int count =1;
-        Map<String , Integer> wordCount = new HashMap<>();
-        ArrayList<String>[] arr = new ArrayList[500];
+         public List<String> topKFrequent(String[] words, int k) {
+        int count = 1;
+        Map<String, Integer> wordCount = new HashMap<>();
+        ArrayList<String>[] arr = new ArrayList[words.length + 1];
         List<String> res = new ArrayList<>();
 
-        Comparator <String> comp = new Comparator<String>() {
+        Comparator<String> comp = new Comparator<String>() {
             @Override
             public int compare(String s1, String s2) {
                 return s2.compareTo(s1);
             }
-          };
-          Arrays.sort(words,comp);
+        };
+        Arrays.sort(words, comp);
 
-          for(int i =0;i<words.length;i++){
+        for (int i = 0; i < words.length; i++) {
             System.out.println(words[i]);
-          }
+        }
 
-        for(int i =0;i<words.length-1;i++){
-            if (words[i].equals(words[i+1])) {
+        for (int i = 0; i < words.length - 1; i++) {
+            if (words[i].equals(words[i + 1])) {
                 count++;
-                if(i==words.length-2){
-                wordCount.put(words[i+1],count);
+                if (i == words.length - 2) {
+                    wordCount.put(words[i + 1], count);
+                }
+            } else {
+                wordCount.put(words[i], count);
+                count = 1;
+                if (i == words.length - 2) {
+                    wordCount.put(words[i + 1], 1);
                 }
             }
-            else{
-                wordCount.put(words[i],count);
-                count =1;
-                if(i==words.length-2){
-                    wordCount.put(words[i+1],1);
-                    }
-            }    
-          }
-          wordCount.forEach((key, value) -> { 
-        if (arr[value] == null) {
+        }
+        System.out.println(wordCount);
+
+        wordCount.forEach((key, value) -> {
+            if (arr[value] == null) {
                 arr[value] = new ArrayList<>();
             }
             arr[value].add(key);
-});
+            Collections.sort(arr[value]);
+        });
 
+        
 
-
-    for(int i =499;i>=0;i--){
-        if(arr[i]!=null){
-            Collections.sort(arr[i]);
-            res.addAll(arr[i]);
+        for (int i = arr.length - 1; i >= 0 && res.size() < k; i--) {
+            if (arr[i] != null) {
+                res.addAll(arr[i]);
+            }
         }
-    }  
-          return(res.subList(0, k));
+
+        return res.subList(0, k);
     }
 }
