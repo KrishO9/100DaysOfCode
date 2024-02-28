67ed88377b03077b04e3f66eed8ebdb5148c38a2
KrishO9 <84735625+KrishO9@users.noreply.github.com>
2024-02-28 11:45:14 +0530

    Time: 6 ms (98.19%), Space: 44.5 MB (73.29%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,59 +1,33 @@
-import java.util.List;
-import java.util.Arrays;
-import java.util.*;
-import java.util.ArrayList;
-import java.util.Comparator;
 class Solution {
-         public List<String> topKFrequent(String[] words, int k) {
-        int count = 1;
-        Map<String, Integer> wordCount = new HashMap<>();
-        ArrayList<String>[] arr = new ArrayList[words.length + 1];
-        List<String> res = new ArrayList<>();
-
-        Comparator<String> comp = new Comparator<String>() {
-            @Override
-            public int compare(String s1, String s2) {
-                return s2.compareTo(s1);
-            }
-        };
-        Arrays.sort(words, comp);
-
-        for (int i = 0; i < words.length; i++) {
-            System.out.println(words[i]);
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String i : words) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
-
-        for (int i = 0; i < words.length - 1; i++) {
-            if (words[i].equals(words[i + 1])) {
-                count++;
-                if (i == words.length - 2) {
-                    wordCount.put(words[i + 1], count);
-                }
-            } else {
-                wordCount.put(words[i], count);
-                count = 1;
-                if (i == words.length - 2) {
-                    wordCount.put(words[i + 1], 1);
-                }
-            }
-        }
-        System.out.println(wordCount);
-
-        wordCount.forEach((key, value) -> {
-            if (arr[value] == null) {
-                arr[value] = new ArrayList<>();
-            }
-            arr[value].add(key);
-            Collections.sort(arr[value]);
-        });
-
         
-
-        for (int i = arr.length - 1; i >= 0 && res.size() < k; i--) {
-            if (arr[i] != null) {
-                res.addAll(arr[i]);
+        List<String>[] arr = new List[words.length + 1];
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (arr[entry.getValue()] == null) {
+                arr[entry.getValue()] = new ArrayList<>();
+            }
+            arr[entry.getValue()].add(entry.getKey());
+        }
+        
+        List<String> output = new ArrayList<>();
+        for (int i = words.length; i >= 0 && k > 0; i--) {
+            if (arr[i] == null) {
+                continue;
+            } else {
+                Collections.sort(arr[i]);
+                for (String j : arr[i]) {
+                    output.add(j);
+                    k--;
+                    if (k == 0) {
+                        break;
+                    }
+                }
             }
         }
-
-        return res.subList(0, k);
+        return output;
     }
 }
