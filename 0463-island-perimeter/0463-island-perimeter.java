b13a0cf36da05ffeda4078b61b7bedc474a69166
KrishO9 <84735625+KrishO9@users.noreply.github.com>
2024-02-29 08:06:21 +0530

    Time: 4 ms (99.87%), Space: 44.9 MB (95.73%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int islandPerimeter(int[][] grid) {
+   /* public int islandPerimeter(int[][] grid) {
         if(grid.length==1&&grid[0].length==1){return 4;}
         int perimeter = 0;
         for(int i =0;i<grid.length;i++)
@@ -34,5 +34,27 @@ class Solution {
         }
 
         return  perimeter;
+    } */
+    
+    public int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    perimeter += 4;
+
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        perimeter -= 2;
+                    }
+
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        perimeter -= 2;
+                    }
+                }
+            }
+        }
+
+        return perimeter;
     }
 }
