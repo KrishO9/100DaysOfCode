e85b1cd7726e0e9a79613ee55051e85f1f008d35
KrishO9 <84735625+KrishO9@users.noreply.github.com>
2024-03-01 22:18:44 +0530

    Time: 2 ms (83.47%), Space: 43.8 MB (30.22%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String maximumOddBinaryNumber(String s) {
+   /* public String maximumOddBinaryNumber(String s) {
         int i = s.length()-1;
         int count = 0;
         while(i>=0)
@@ -25,6 +25,30 @@ class Solution {
                 count--;
             }
         }
+        return sb.toString();
+        
+    } */
+    
+     public String maximumOddBinaryNumber(String s) {
+        int i = s.length()-1;
+        int count1 = 0;
+        int count0 = 0;
+        while(i>=0)
+        {
+            if(s.charAt(i)=='1')
+            {
+                count1++;
+            }
+            else{
+                count0++;
+            }
+            i--;
+        }
+        StringBuilder sb = new StringBuilder("");
+        sb.append("1".repeat(count1-1));
+        sb.append("0".repeat(count0));
+        sb.append("1");
+        
         return sb.toString();
         
     }
