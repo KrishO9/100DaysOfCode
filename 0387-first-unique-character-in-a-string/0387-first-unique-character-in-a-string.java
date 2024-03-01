8a3e50a2a649e991907150f96d7b1397231035fe
KrishO9 <84735625+KrishO9@users.noreply.github.com>
2024-03-01 22:54:47 +0530

    Time: 34 ms (13.36%), Space: 45 MB (44.33%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -8,12 +8,12 @@ class Solution {
         for (char c : s.toCharArray()) {
             frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
         }
+
         for (int i = 0; i < s.length(); i++) {
             if (frequencyMap.get(s.charAt(i)) == 1) {
                 return i;
             }
         }
-
         return -1; 
     }
 }
